cd990eca013c6981e1876df2ef868708dc15bbe9
Tushar <beingtushar2001@gmail.com>
2021-09-13 13:28:18 +0530

    first commit

---
+++ Covid.js
@@ -0,0 +1,18 @@
+$(document).ready(function(){
+
+    $('#menu').click(function(){
+        $(this).toogleClass('fa-times');
+        $('.navbar').toggleClass('nav-toggle');
+    });
+
+    $(window).on('load scroll',function(){
+        $('#menu').removeClass('fa-times');
+        $('.navbar').removeClass('nav-toggle'); 
+
+        if($(window).scrollTop()>0){
+            $('header').addClass('sticky');
+        }else{
+            $('header').removeClass('sticky');
+        }
+    });
+});
